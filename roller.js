c5436830b4c33a0b6b747946ce6a88296b149002
Wait. I'm thinking... <al.neodim@gmail.com>
2015-07-25 15:16:34 +0300

    Update roller.js

--- roller.js
+++ roller.js
@@ -1,1 +1,1 @@
-var roller = function (arr) {return arr.map && [].concat.apply([],arr.map(steamroller)) || arr;}; 
+var roller = function (arr) {return arr.map && [].concat.apply([],arr.map(roller)) || arr;}; 
